79952ac66eb931ff147088d2c3974ac75a31bcb3
Viktor Hristov <virto.bulgaria@gmail.com>
2025-06-21 19:31:25 +0300

    Add card component

--- src/app/toolbars/toolbar-three/toolbar-w-three.component.ts
+++ src/app/toolbars/toolbar-three/toolbar-w-three.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DynamicComponentRenderer } from '../../widget/dynamic-renderer.component';
 import { TOOLBAR_COMPONENTS } from '../../widget/toolbar.registry';
 import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'toolbar-three',
   templateUrl: './toolbar-w-three.component.html',
-  imports: [ToolbarModule],
+  imports: [ToolbarModule, CardModule],
 })
 export class ToolbarWidgetThreeComponent extends DynamicComponentRenderer {
   protected getComponentRegistry() {
--- src/app/toolbars/toolbar-two/toolbar-w-two.component.ts
+++ src/app/toolbars/toolbar-two/toolbar-w-two.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DynamicComponentRenderer } from '../../widget/dynamic-renderer.component';
 import { TOOLBAR_COMPONENTS } from '../../widget/toolbar.registry';
 import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'toolbar-two',
   templateUrl: './toolbar-w-two.component.html',
-  imports: [ToolbarModule],
+  imports: [CardModule, ToolbarModule],
 })
 export class ToolbarWidgetTwoComponent extends DynamicComponentRenderer {
   protected getComponentRegistry() {
